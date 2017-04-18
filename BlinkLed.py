3d7d55de1d1784e5e983901b9edb935dbc4442b3
Brian DuVerneay <duverneay@gmail.com>
2017-04-18 22:55:36 +0000

    feat: Add tkinter button to turn off/on LED blink
    Use an additional thread for the LED blink

--- BlinkLed.py
+++ BlinkLed.py
@@ -1,5 +1,9 @@
 import RPi.GPIO as GPIO
 import time
+import threading
+from tkinter import *
+
+blinkState = False;
 
 # set Pin Numbering Mode
 GPIO.setmode(GPIO.BOARD) # numbering is board based numbering
@@ -8,11 +12,27 @@ GPIO.setmode(GPIO.BOARD) # numbering is board based numbering
 # Set Pin Direction and Assignment
 GPIO.setup(13, GPIO.OUT)
 
-while True:
-	# Assign value to output pin
-	GPIO.output(13, True)
-	time.sleep(1)
-	GPIO.output(13, False)
-	time.sleep(1)
 
+def buttonPress():
+	global blinkState
+	blinkState = not blinkState
+	print ("blink state: " + str(blinkState))	
+	t=threading.Thread(target=LEDblink)
+	t.start()
+	
+def LEDblink():
+	while blinkState:
+		# Assign value to output pin
+		if blinkState:
+			GPIO.output(13, True)
+			time.sleep(1)
+			GPIO.output(13, False)
+			time.sleep(1)
+		else:
+			GPIO.output(13, False)
+
+root = Tk()
+b = Button(root, text="blink start/stop", command=buttonPress)
+b.pack()
+root.mainloop()
 
